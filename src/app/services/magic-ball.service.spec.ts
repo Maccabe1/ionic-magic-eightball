e52ac2001f2076563dc4a029fbdcfc0b31e02e2c
Maccabe1 <gdmaccabe@hotmail.co.uk>
2020-08-27 21:37:31 +0300

    Add tests for return random answer and empty results

--- src/app/services/magic-ball.service.spec.ts
+++ src/app/services/magic-ball.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { MagicBallService } from './magic-ball.service';
 
+
 describe('MagicBallService', () => {
   let service: MagicBallService;
 
@@ -13,4 +14,29 @@ describe('MagicBallService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe("Magic 8 Ball Service", () => {
+    beforeEach(() => {
+      magicBall = new MagicBallService();
+    });
+  
+    it("should return a non empty array", () => {
+      let result = magicBall.getAnswers();
+  
+      expect(Array.isArray(result)).toBeTruthy;
+      expect(result.length).toBeGreaterThan(0);
+    });
+  
+    it("should return one random answer as a string", () => {
+      expect(typeof magicBall.getRandomAnswer()).toBe("string");
+    });
+  
+    it("should have both yes and no available in result set", () => {
+      let result = magicBall.getAnswers();
+  
+      expect(result).toContain("Yes");
+      expect(result).toContain("No");
+    });
+  });
+
 });
